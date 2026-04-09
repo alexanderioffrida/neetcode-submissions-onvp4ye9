dcfd57cc5a3d399cb0064a729ab409646e8c09c0
alexanderioffrida <zanderflores05@gmail.com>
2026-04-09 09:30:32 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
