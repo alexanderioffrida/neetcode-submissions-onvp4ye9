f3e8f10a58e3249eaffc99ed18ae6c64f6e56ccf
alexanderioffrida <zanderflores05@gmail.com>
2026-04-14 10:13:20 -0400

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.difference = 0
+
+        def maxHeight(root: Optional[TreeNode]) -> int:
+            if not root: return 0
+
+            leftHeight = maxHeight(root.left)
+            rightHeight = maxHeight(root.right)
+
+            self.difference = max(self.difference, abs(leftHeight - rightHeight))
+
+            return 1 + max(leftHeight, rightHeight)
+        
+        maxHeight(root)
+        return self.difference <= 1
